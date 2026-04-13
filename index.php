a3dc98399b202fb23f0fcb7fc50d0b88cb8c9cb0
7220B <bah.abdoulaye.sio1t@gmail.com>
2026-04-13 19:35:32 +0200

    Mis à jours le chemin vers le css

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $taches = $gestionnaire->getMesTaches();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Todo List</title>
 
 </head>
